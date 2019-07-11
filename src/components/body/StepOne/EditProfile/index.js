f221aa5a87a04447e9b1632970642689f61d4a08
SC-NLOPEZ-OSX <sc-nlopez-osx@SC-NLOPEZ-OSX.local>
2019-07-11 10:28:11 -0500

    edit profile started

---
+++ src/components/body/StepOne/EditProfile/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  './editProfile.scss'
+
+class EditProfile extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        return  <div className="editProfile">
+
+                </div>
+
+    }
+    
+}
